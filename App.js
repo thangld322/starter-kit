214a00a6582de8e302c22e292ecf0c875e50ff08
thangld <thangld@smartosc.com>
2022-10-20 14:45:17 +0700

    glb

--- App.js
+++ App.js
@@ -42,54 +42,88 @@ var HelloWorldSceneAR = createReactClass({
 
   render: function() {
     return (
+      // <ViroARScene>
+
+      //   <ViroLightingEnvironment source={require('./res/tesla/garage_1k.hdr')}/>
+
+      //   <ViroARImageMarker target={"logo"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
+      //     <ViroNode scale={[0, 0, 0]} transformBehaviors={["billboardY"]} animation={{name:this.state.animName, run:this.state.playAnim,}}>
+      //       <ViroSphere materials={["white_sphere"]}
+      //         heightSegmentCount={20} widthSegmentCount={20} radius={.03}
+      //         position={[-.2, .25, 0]}
+      //         onClick={this._selectWhite}
+      //         animation={{name:"tapAnimation", run:this.state.tapWhite, onFinish:this._animateFinished}}
+      //         shadowCastingBitMask={0} />
+
+      //       <ViroSphere materials={["blue_sphere"]}
+      //         heightSegmentCount={20} widthSegmentCount={20} radius={.03}
+      //         position={[-.1, .25, 0]}
+      //         onClick={this._selectBlue}
+      //         animation={{name:"tapAnimation", run:this.state.tapBlue, onFinish:this._animateFinished}}
+      //         shadowCastingBitMask={0} />
+
+      //       <ViroSphere materials={["grey_sphere"]}
+      //         heightSegmentCount={20} widthSegmentCount={20} radius={.03}
+      //         position={[0, .25, 0]}
+      //         onClick={this._selectGrey}
+      //         animation={{name:"tapAnimation", run:this.state.tapGrey, onFinish:this._animateFinished}}
+      //         shadowCastingBitMask={0} />
+
+      //       <ViroSphere materials={["red_sphere"]}
+      //         heightSegmentCount={20} widthSegmentCount={20} radius={.03}
+      //         position={[.1, .25, 0]}
+      //         onClick={this._selectRed}
+      //         animation={{name:"tapAnimation", run:this.state.tapRed, onFinish:this._animateFinished}}
+      //         shadowCastingBitMask={0} />
+
+      //       <ViroSphere materials={["yellow_sphere"]}
+      //         heightSegmentCount={20} widthSegmentCount={20} radius={.03}
+      //         position={[.2, .25, 0]}
+      //         onClick={this._selectYellow}
+      //         animation={{name:"tapAnimation", run:this.state.tapYellow, onFinish:this._animateFinished}}
+      //         shadowCastingBitMask={0}/>
+      //     </ViroNode>
+
+      //     <Viro3DObject
+      //       scale={[0, 0, 0]}
+      //       source={require('./res/tesla/object_car.obj')}
+      //       resources={[require('./res/tesla/object_car.mtl'),
+      //                   ]}
+      //       type="OBJ"
+      //       materials={this.state.texture}
+      //       onClick={this._toggleButtons}
+      //       animation={{name:"scaleCar", run:this.state.animateCar,}} />
+
+      //     <ViroSpotLight
+      //       innerAngle={5}
+      //       outerAngle={25}
+      //       direction={[0,-1,0]}
+      //       position={[0, 5, 1]}
+      //       color="#ffffff"
+      //       castsShadow={true}
+      //       shadowMapSize={2048}
+      //       shadowNearZ={2}
+      //       shadowFarZ={7}
+      //       shadowOpacity={.7} />
+
+      //     <ViroQuad
+      //       rotation={[-90, 0, 0]}
+      //       position={[0, -0.001, 0]}
+      //       width={2.5} height={2.5}
+      //       arShadowReceiver={true} />
+
+      //   </ViroARImageMarker>
+      // </ViroARScene>
+
       <ViroARScene>
 
         <ViroLightingEnvironment source={require('./res/tesla/garage_1k.hdr')}/>
 
         <ViroARImageMarker target={"logo"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
-          <ViroNode scale={[0, 0, 0]} transformBehaviors={["billboardY"]} animation={{name:this.state.animName, run:this.state.playAnim,}}>
-            <ViroSphere materials={["white_sphere"]}
-              heightSegmentCount={20} widthSegmentCount={20} radius={.03}
-              position={[-.2, .25, 0]}
-              onClick={this._selectWhite}
-              animation={{name:"tapAnimation", run:this.state.tapWhite, onFinish:this._animateFinished}}
-              shadowCastingBitMask={0} />
-
-            <ViroSphere materials={["blue_sphere"]}
-              heightSegmentCount={20} widthSegmentCount={20} radius={.03}
-              position={[-.1, .25, 0]}
-              onClick={this._selectBlue}
-              animation={{name:"tapAnimation", run:this.state.tapBlue, onFinish:this._animateFinished}}
-              shadowCastingBitMask={0} />
-
-            <ViroSphere materials={["grey_sphere"]}
-              heightSegmentCount={20} widthSegmentCount={20} radius={.03}
-              position={[0, .25, 0]}
-              onClick={this._selectGrey}
-              animation={{name:"tapAnimation", run:this.state.tapGrey, onFinish:this._animateFinished}}
-              shadowCastingBitMask={0} />
-
-            <ViroSphere materials={["red_sphere"]}
-              heightSegmentCount={20} widthSegmentCount={20} radius={.03}
-              position={[.1, .25, 0]}
-              onClick={this._selectRed}
-              animation={{name:"tapAnimation", run:this.state.tapRed, onFinish:this._animateFinished}}
-              shadowCastingBitMask={0} />
-
-            <ViroSphere materials={["yellow_sphere"]}
-              heightSegmentCount={20} widthSegmentCount={20} radius={.03}
-              position={[.2, .25, 0]}
-              onClick={this._selectYellow}
-              animation={{name:"tapAnimation", run:this.state.tapYellow, onFinish:this._animateFinished}}
-              shadowCastingBitMask={0}/>
-          </ViroNode>
-
           <Viro3DObject
             scale={[0, 0, 0]}
-            source={require('./res/tesla/object_car.obj')}
-            resources={[require('./res/tesla/object_car.mtl'),
-                        ]}
-            type="OBJ"
+            source={{ uri: "https://ipfs.io/ipfs/QmPute6BHeBUVsccWJ5m5gPcqB8Up3v5am5ahZZX3jNT76"}}
+            type="GLB"
             materials={this.state.texture}
             onClick={this._toggleButtons}
             animation={{name:"scaleCar", run:this.state.animateCar,}} />
